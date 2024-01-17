ba76aa548e6a4c1880ae767ff29477f80f9b6240
giusmili <giusmili67@gmail.com>
2024-01-17 18:42:07 +0100

    optimization php

---
+++ index.php
@@ -0,0 +1,49 @@
+<!-- header -->
+<?php
+    include_once "./src/header.inc.php";
+?>
+<body>
+    <header>
+        <h1><?= title ?></h1>
+    </header>
+    <picture>
+        <source srcset="./asset/cover.png"
+                media="(orientation: portrait)">
+        <img src="./asset/cover.png" alt="cover" loading="lazy">
+    </picture>
+    <main>
+        <!-- partie dynamique: date et version -->
+      
+        <h2>Nous sommes le 
+          <time datetime="<?= $_datetime ?>"><?= $_date_php ?></time> 
+        </h2>
+            <p>
+           
+
+                Mise à jour PHP <?= $_modif_php ?> <br>
+            </p>
+        <!-- end -->
+        <!-- structure d'affichage pour les articles -->
+        
+    <?php
+        include_once "./src/connect.inc.php";
+    ?>
+   <!--end -->
+    <nav>
+        <a class="button" href="search.php">
+            Rechercher les articles
+        </a>
+     
+      
+
+    </nav>
+    </main>
+    <footer>
+        <p>&copy; - MIT - <time 
+        datetime="<?= $_datetime ?>">
+        <?= $_date_php ?>
+        </time></p>
+    </footer>
+   
+</body>
+</html>
